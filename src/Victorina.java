47e298f4cde0184e87b0af0aaf7dedd4ed235789
Saheme <roj_t@mail.ru>
2022-05-04 01:13:11 +0300

    финальная версия ДЗ

--- src/Victorina.java
+++ src/Victorina.java
@@ -6,19 +6,13 @@ public class Victorina {
     public static void main(String[] args) {
         int rightAnswer = 0;
         int wrongAnswer = 0;
-        String [] question = {"Выберите столицу Канады", "С каким океаном не граничит Канада", "Самая длинная река Канады", "Самая высокая гора в Канаде"};
-        for(int i=0;question.length>i;i++){
-            System.out.println(question[i]);
-            System.out.println();
-            String[] variant = {"Ванкувер", "Торонто", "Оттава","Монреаль","Атлантический океан", "Тихий океан", "Южный океан", "Северный Ледовитый океан","Юкон", "Колумбия", "Маккензи", "Пис","Вуд", "Ванкувер", "Кинг Пик", "Логан"};
-            for (int j = 0; variant.length > j; j=j+1) {
-                System.out.println(variant[j]);
-                System.out.println();
-            }
-            String[] answer = {"Оттава", "Южный океан", "Юкон", "Логан"};
-            for (int k = 0; answer.length > k; k = k + 1) {
-                System.out.println(variant[k]);
-            }
+        String capital = "Выберите столицу Канады";
+        System.out.println(capital);
+        System.out.println();
+        String[] variant = {"Ванкувер ", "Торонто ", "Оттава ", "Монреаль "};
+        for (int i = 0; variant.length > i; i++) {
+           int q=i+1;
+            System.out.println(q+" "+ variant[i]);
         }
         Scanner scan = new Scanner(System.in);
         int answer = scan.nextInt();
@@ -28,19 +22,18 @@ public class Victorina {
         } else {
             System.out.println("Неправильно. ");
             wrongAnswer++;
-        }System.out.println("Правильных ответов " + rightAnswer);
-        System.out.println("Неправильных ответов " + wrongAnswer);
+        }
         System.out.println();
 
-       /* String ocean = "С каким океаном не граничит Канада";
+        String ocean = "С каким океаном не граничит Канада";
         System.out.println(ocean);
         System.out.println();
-        String[] variant2 = {"", "Атлантический океан ", "Тихий океан ", "Южный океан ", "Северный Ледовитый океан "};
-        for (int j = 1; variant2.length > j; j++) {
-            System.out.println(variant2[j] + j);
+        String[] variant2 = {"Атлантический океан", "Тихий океан", "Южный океан", "Северный Ледовитый океан"};
+        for (int j = 0; variant2.length > j; j++) {
+            int q=j+1;
+            System.out.println(q+" "+variant2[j]);
         }
-        Scanner scan2 = new Scanner(System.in);
-        int answer2 = scan2.nextInt();
+        int answer2 = scan.nextInt();
         if (answer2 == 3) {
             System.out.println("Вы абсолютно правы! ");
             rightAnswer++;
@@ -53,12 +46,12 @@ public class Victorina {
         String river = "Самая длинная река Канады";
         System.out.println(river);
         System.out.println();
-        String[] variant3 = {"", "Юкон ", "Колумбия ", "Маккензи ", "Пис "};
-        for (int k = 1; variant3.length > k; k++) {
-            System.out.println(variant3[k] + k);
+        String[] variant3 = {"Юкон", "Колумбия", "Маккензи", "Пис"};
+        for (int k = 0; variant3.length > k; k++) {
+            int q=k+1;
+            System.out.println(q+ " "+variant3[k]);
         }
-        Scanner scan3 = new Scanner(System.in);
-        int answer3 = scan3.nextInt();
+        int answer3 = scan.nextInt();
         if (answer3 == 1) {
             System.out.println("Вы абсолютно правы! ");
             rightAnswer++;
@@ -71,12 +64,12 @@ public class Victorina {
         String mountain = "Самая высокая гора в Канаде";
         System.out.println(mountain);
         System.out.println();
-        String[] variant4 = {"", "Вуд ", "Ванкувер ", "Кинг Пик ", "Логан "};
-        for (int m = 1; variant4.length > m; m++) {
-            System.out.println(variant4[m] + m);
+        String[] variant4 = {"Вуд", "Ванкувер", "Кинг Пик", "Логан"};
+        for (int m = 0; variant4.length > m; m++) {
+            int q=m+1;
+            System.out.println(q+" "+variant4[m]);
         }
-        Scanner scan4 = new Scanner(System.in);
-        int answer4 = scan4.nextInt();
+        int answer4 = scan.nextInt();
         if (answer4 == 4) {
             System.out.println("Вы абсолютно правы! ");
             rightAnswer++;
@@ -85,9 +78,34 @@ public class Victorina {
             wrongAnswer++;
         }
         System.out.println();
-         */
+        System.out.println("Правильных ответов " + rightAnswer);
+        System.out.println("Неправильных ответов " + wrongAnswer);
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
